a6449ca186c279f8ae99621b359915f83dba5e6f
Jay <yeonjae5252@gmail.com>
2018-10-12 23:27:25 +0900

    ambient lighting 광원 및 카메라 위치 변경

--- Lighting/AmbientLighting/Game.cs
+++ Lighting/AmbientLighting/Game.cs
@@ -57,79 +57,96 @@ namespace AmbientLigthing
         };
 
         uint VBO;
-        uint cubeVAO;
-        uint lightVAO;
-        Shader shader;
-        Shader lightShader;
+        uint lampVAO;
+        uint modelVAO;
+
+        Shader lampShader;
+        Shader modelShader;
+
+        Vector3 lightPos = new Vector3(1.2f, 3.0f, 2.0f);
 
         Matrix4 model;
         Matrix4 view;
         Matrix4 projection;
-        float timeValue;
+
+        private const string uniformModel = "model";
+        private const string uniformView = "view";
+        private const string uniformProjection = "projection";
 
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
 
-            lightShader = new Shader(@"vertexShader.vs", @"lightShader.fs");
-            lightShader.Create();
+            modelShader = new Shader(@"vertexShader.vs", @"ambientShader.fs");
+            modelShader.Create();
 
-            shader = new Shader(@"vertexShader.vs", @"fragShader.fs");
-            shader.Create();
+            lampShader = new Shader(@"vertexShader.vs", @"fragShader.fs");
+            lampShader.Create();
 
-            GL.GenVertexArrays(1, out cubeVAO);
-            GL.BindVertexArray(cubeVAO);
+            // model
+            GL.GenVertexArrays(1, out modelVAO);
+            GL.BindVertexArray(modelVAO);
 
             GL.GenBuffers(1, out VBO);
             GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
             GL.BufferData(BufferTarget.ArrayBuffer, sizeof(float) * vertices.Length, vertices, BufferUsageHint.StaticDraw);
 
-            // position attribute
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
 
-            GL.GenVertexArrays(1, out lightVAO);
-            GL.BindVertexArray(lightVAO);
+            // lamp
+            GL.GenVertexArrays(1, out lampVAO);
+            GL.BindVertexArray(lampVAO);
             GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
 
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
         }
 
-        protected override void OnRenderFrame(FrameEventArgs e)
+        protected override void OnResize(EventArgs e)
         {
-            base.OnRenderFrame(e);
-            
-            GL.Clear(ClearBufferMask.ColorBufferBit);
+            base.OnResize(e);
 
-            lightShader.UseProgram();
-            lightShader.SetVec3("objectColor", 1.0f, 0.5f, 0.31f);
-            lightShader.SetVec3("lightColor", 1.0f, 1.0f, 1.0f);
-
-            model = Matrix4.CreateFromAxisAngle(new Vector3(0.5f, 1.0f, 0.0f), (float)(50.0f * Math.PI / 180));
-            view = Matrix4.CreateTranslation(0.0f, 0.0f, -5f);
-
+            // projection
             float fov = (float)(45.0f * Math.PI / 180);
             float aspectRatio = Width / Height;
             projection = Matrix4.CreatePerspectiveFieldOfView(fov, aspectRatio, 0.1f, 100.0f);
+        }
 
-            GL.UniformMatrix4(GL.GetUniformLocation(lightShader.Id, "model"), false, ref model);
-            GL.UniformMatrix4(GL.GetUniformLocation(lightShader.Id, "view"), false, ref view);
-            GL.UniformMatrix4(GL.GetUniformLocation(lightShader.Id, "projection"), false, ref projection);
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
 
-            GL.BindVertexArray(lightVAO);
+            GL.Enable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.LineSmooth);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            // model(cube) object
+            modelShader.UseProgram();
+            modelShader.SetVec3("objectColor", 1.0f, 0.5f, 0.31f);
+            modelShader.SetVec3("lightColor", 1.0f, 1.0f, 1.0f);
+
+            model = Matrix4.Identity;
+            view = Matrix4.LookAt(new Vector3(-1.5f, 1.5f, -3f), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+
+            modelShader.SetMat4(uniformModel, model);
+            modelShader.SetMat4(uniformView, view);
+            modelShader.SetMat4(uniformProjection, projection);
+
+            GL.BindVertexArray(modelVAO);
             GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
 
-            shader.UseProgram();
-            model *= Matrix4.CreateTranslation(5, 0, 5);
-            model *= Matrix4.CreateScale(0.2f);
-            GL.UniformMatrix4(GL.GetUniformLocation(shader.Id, "model"), false, ref model);
-            GL.UniformMatrix4(GL.GetUniformLocation(shader.Id, "view"), false, ref view);
-            GL.UniformMatrix4(GL.GetUniformLocation(shader.Id, "projection"), false, ref projection);
+            // lamp object
+            lampShader.UseProgram();
+            model *= Matrix4.CreateTranslation(lightPos);
+            model *= Matrix4.CreateScale(0.3f);
 
-            GL.BindVertexArray(cubeVAO);
+            lampShader.SetMat4(uniformModel, model);
+            lampShader.SetMat4(uniformView, view);
+            lampShader.SetMat4(uniformProjection, projection);
+
+            GL.BindVertexArray(lampVAO);
             GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
-            //GL.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, 0);
 
             SwapBuffers();
         }
