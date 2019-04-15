213482677827c7e4327777511737afbf0ee0b4c1
=oksujay1127 <=oksujay1127@gmail.com>
2019-04-15 18:01:18 +0900

    add framebuffer project

---
+++ FrameBuffer/FrameBufferScene.cs
@@ -0,0 +1,301 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Toolkit;
+using Imaging = System.Drawing.Imaging;
+
+namespace FrameBuffer
+{
+    class FrameBufferScene : GameWindow
+    {
+        float[] cubeVertices =
+        {
+            // positions          // texture Coords
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+        };
+
+        float[] planeVertices =
+        {
+            // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
+             5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+            -5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+            -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+             5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+            -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+             5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+        };
+
+        float[] quadVertices =
+        {
+             //positions    texCoords
+            -1.0f,  1.0f,  0.0f, 1.0f,
+            -1.0f, -1.0f,  0.0f, 0.0f,
+             1.0f, -1.0f,  1.0f, 0.0f,
+
+            -1.0f,  1.0f,  0.0f, 1.0f,
+             1.0f, -1.0f,  1.0f, 0.0f,
+             1.0f,  1.0f,  1.0f, 1.0f
+        };
+
+        int cubeVAO;
+        int cubeVBO;
+        int planeVAO;
+        int planeVBO;
+        int quadVAO;
+        int quadVBO;
+
+        uint cubeTextureId;
+        uint planeTextureId;
+
+        int framebuffer;
+        uint textureColorBuffer;
+        uint rbo;
+
+        Shader shader;
+        Shader screenShader;
+        FPSCamera camera = new FPSCamera(new Vector3(0, 0, 5));
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            shader = new Shader(@"frameBufferShader.vs", @"frameBufferShader.fs");
+            shader.Create();
+
+            screenShader = new Shader(@"screenShader.vs", @"screenShader.fs");
+            screenShader.Create();
+
+            // cube            
+            GL.GenVertexArrays(1, out cubeVAO);
+            GL.BindVertexArray(cubeVAO);
+
+            GL.GenBuffers(1, out cubeVBO);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, cubeVBO);
+            GL.BufferData(BufferTarget.ArrayBuffer, sizeof(float) * cubeVertices.Length, cubeVertices, BufferUsageHint.StaticDraw);
+
+            // position
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            // texture
+            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+
+            // floor            
+            GL.GenVertexArrays(1, out planeVAO);
+            GL.BindVertexArray(planeVAO);
+
+            GL.GenBuffers(1, out planeVBO);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, planeVBO);
+            GL.BufferData(BufferTarget.ArrayBuffer, sizeof(float) * planeVertices.Length, planeVertices, BufferUsageHint.StaticDraw);
+
+            // position
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            // texture
+            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+
+            // quad
+            GL.GenVertexArrays(1, out quadVAO);
+            GL.BindVertexArray(quadVAO);
+
+            GL.GenBuffers(1, out quadVBO);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, quadVBO);
+            GL.BufferData(BufferTarget.ArrayBuffer, sizeof(float) * quadVertices.Length, quadVertices, BufferUsageHint.StaticDraw);
+
+            //position
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 4 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            //texture
+            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 4 * sizeof(float), 2 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+
+            // texture load
+            cubeTextureId = LoadTexture(@"Resources\marble.jpg");
+            planeTextureId = LoadTexture(@"Resources\metal.png");
+
+            shader.UseProgram();
+            shader.SetInt("texture1", 0);
+
+            screenShader.UseProgram();
+            screenShader.SetInt("screenTexture", 0);
+
+            //framebuffer
+            GL.GenFramebuffers(1, out framebuffer);
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, framebuffer);
+
+            GL.GenTextures(1, out textureColorBuffer);
+            GL.BindTexture(TextureTarget.Texture2D, textureColorBuffer);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, Width, Height, 0, PixelFormat.Rgb, PixelType.UnsignedByte, IntPtr.Zero);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            GL.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2D, textureColorBuffer, 0);
+
+            GL.GenRenderbuffers(1, out rbo);
+            GL.BindRenderbuffer(RenderbufferTarget.Renderbuffer, rbo);
+            GL.RenderbufferStorage(RenderbufferTarget.Renderbuffer, RenderbufferStorage.Depth24Stencil8, Width, Height);
+            GL.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferAttachment.DepthStencilAttachment, RenderbufferTarget.Renderbuffer, rbo);
+
+            if (GL.CheckFramebufferStatus(FramebufferTarget.Framebuffer) != FramebufferErrorCode.FramebufferComplete)
+            {
+
+            }
+
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, framebuffer);
+
+            //GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+            GL.Enable(EnableCap.DepthTest);
+
+            GL.ClearColor(0.1f, 0.1f, 0.1f, 0.1f);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            shader.UseProgram();
+
+            var view = camera.ViewMatrix;
+            var projection = Matrix4.CreatePerspectiveFieldOfView((float)(45.0f * Math.PI / 180),
+                                                                  Width / Height, 0.1f, 100.0f);
+
+            shader.SetMat4("view", view);
+            shader.SetMat4("projection", projection);
+
+            // cubes
+            GL.BindVertexArray(cubeVAO);
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, cubeTextureId);
+
+            var model = Matrix4.CreateTranslation(-1.0f, 0.0f, -1.0f);
+            shader.SetMat4("model", model);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
+
+            model = Matrix4.CreateTranslation(2.0f, 0.0f, 0.0f);
+            shader.SetMat4("model", model);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
+
+            // plane
+            GL.BindVertexArray(planeVAO);
+            GL.BindTexture(TextureTarget.Texture2D, planeTextureId);
+            shader.SetMat4("model", Matrix4.Identity);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+
+            // framebuffer
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+            GL.Disable(EnableCap.DepthTest);
+            GL.ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            screenShader.UseProgram();
+
+            GL.BindVertexArray(quadVAO);
+            GL.BindTexture(TextureTarget.Texture2D, textureColorBuffer);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+
+            GL.BindVertexArray(0);
+
+            SwapBuffers();
+        }
+
+        protected override void OnKeyDown(KeyboardKeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            // camera position
+            if (e.Key == Key.Up)
+            {
+                camera.MoveForward();
+            }
+            else if (e.Key == Key.Down)
+            {
+                camera.MoveBackward();
+            }
+            else if (e.Key == Key.Left)
+            {
+                camera.MoveLeft();
+            }
+            else if (e.Key == Key.Right)
+            {
+                camera.MoveRight();
+            }
+        }
+
+        private uint LoadTexture(string path)
+        {
+            GL.GenTextures(1, out uint textureId);
+
+            Bitmap bitmap = new Bitmap(path);
+            Imaging.BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                                                        Imaging.ImageLockMode.ReadOnly,
+                                                        Imaging.PixelFormat.Format32bppRgb);
+
+            GL.BindTexture(TextureTarget.Texture2D, textureId);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
+                          bitmap.Width, bitmap.Height, 0,
+                          PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.LinearMipmapLinear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            bitmap.UnlockBits(data);
+            bitmap.Dispose();
+
+            return textureId;
+        }
+    }
+}
